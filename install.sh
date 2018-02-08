eabf2160c3557c356f2fc8ec0f2785d7fc7ea8a3
Chaim Halbert <chalbert@evernote.com>
2018-02-08 13:32:34 -0800

    install.sh: fix appending of repoactions -e to PROMPT_COMMAND

--- install.sh
+++ install.sh
@@ -4,6 +4,20 @@ set -e
 PREFIX="$1"
 profile="$2"
 
+# setup prompt command
+function repoactions_triggers() {
+    cat << 'EOF'
+# BEGIN repoactions triggers
+if [[ -n "$PROMPT_COMMAND" ]] && [[ "$PROMPT_COMMAND" != *; ]]; then
+    PROMPT_COMMAND="${PROMPT_COMMAND}"';eval "$(repoactions -e)"'
+else
+    PROMPT_COMMAND="${PROMPT_COMMAND}"'eval "$(repoactions -e)"'
+fi
+export PROMPT_COMMAND
+# END repoactions
+EOF
+}
+
 if [ -z "$PREFIX" ]; then
     echo "Run this instead:"
     echo ""
@@ -28,27 +42,15 @@ pushd "${PREFIX}/bin" >/dev/null
 ln -s ../libexec/repoactions/repoactions.sh repoactions
 popd >/dev/null
 
-PROMPT_COMMAND='eval "\$(repoactions -e)";'"\${PROMPT_COMMAND}"
 if [ -z "$profile" ]; then
 	echo "We have not updated your shell's rc files."
-	echo "Add this line to your .bashrc:"
+	echo "Add these lines to your .bashrc, or equivalent:"
 	echo ""
-	printf '    PROMPT_COMMAND="%s"\n' "${PROMPT_COMMAND//\"/\\\"}"
-	echo ""
+    repoactions_triggers
 else
-	# setup prompt command
-	cat << EOF >> "$profile"
-# BEGIN repoactions triggers
-if [[ -n "$PROMPT_COMMAND" ]] && [[ "$PROMPT_COMMAND" != *; ]]; then
-    PROMPT_COMMAND="${PROMPT_COMMAND}"';eval "\$(repoactions -e)";'
-else
-    PROMPT_COMMAND="${PROMPT_COMMAND}"'eval "\$(repoactions -e)";'
+    repoactions_triggers >> "$profile"
+    echo "Note: repoactions sets itself up when you open your shell, so a few"
+    echo "lines have been added to"
+    echo "  $profile"
 fi
-export PROMPT_COMMAND
-# END repoactions
-EOF
 
-	echo "Note: repoactions sets itself up when you open your shell, so a few"
-	echo "lines have been added to"
-	echo "  $profile"
-fi
